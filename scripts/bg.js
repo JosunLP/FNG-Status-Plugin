db9b6c4050fcc7fd94eb02ece5e1e5d957f9bc00
JosunLP <Jolun.swtor@gmail.com>
2017-12-21 23:37:54 +0100

    brain fix

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,7 +1,3 @@
-//ROOT SERVER
-function pingReqRoot() {
-
-  var server = ;//beschreibung der Server Adresse
 
   var data = JSON.stringify(false);
 
@@ -14,54 +10,13 @@ function pingReqRoot() {
     }
   });
 
-  xhr.open("GET", "https://fearnixx.de/status/public/api/v1/ping");
+  xhr.open("GET", "https://fearnixx.de/status/public/api/v1/components?sort=status&order=desc");
 
   xhr.send(data);
-}
-//TEAMSPEAK SERVER
-function pingReqTS() {
-
-  var server = ;//beschreibung der Server Adresse
-
-  var data = JSON.stringify(false);
-
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      console.log(this.responseText);
-    }
-  });
-
-  xhr.open("GET", "https://fearnixx.de/status/public/api/v1/ping");
-
-  xhr.send(data);
-}
-
-function pingReqBeispiel() {
-
-  var server = ;//beschreibung der Server Adresse
-
-  var data = JSON.stringify(false);
-
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      console.log(this.responseText);
-    }
-  });
-
-  xhr.open("GET", "https://fearnixx.de/status/public/api/v1/ping");
-
-  xhr.send(data);
-}
 
 
 
-
+//https://fearnixx.de/status/public/api/v1/components?sort=status=asc
 
 
 
