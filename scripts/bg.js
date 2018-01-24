bea346886fcd10063d3ab37560f37e2852fa4304
JosunLP <Jolun.swtor@gmail.com>
2018-01-24 19:53:38 +0100

    n/a

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,52 +1,47 @@
-function getDataInfos() {
-  var data = JSON.stringify(false);
 
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
+var data = JSON.stringify(false);
 
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      console.log(this.responseText);
-      //window.alert(this.responseText);
-    }
-  });
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
 
-
-  xhr.open("GET", "https://fearnixx.de/status/public/api/v1/components?sort=status&order=desc");
-
-  xhr.send(data);
-}
-
-getDataInfos();
-
-
-function postState() {
-
-  //var type ids
-  var stroot = document.getElementById('root');
-  var stts = document.getElementById('ts');
-  var stgame = document.getElementById('game');
-  var stwebsite = document.getElementById('website');
-
-  //var type infos
-  //var clroot = ;
-  //var clts = ;
-  //var clgame = ;
-  //var clwebsite = ;
-
-
-  console.log(stroot.innerHTML, stts.innerHTML, stgame.innerHTML, stwebsite.innerHTML);
-
+xhr.addEventListener("readystatechange", function () {
   if (this.readyState === this.DONE) {
-    stroot.innerHTML = "Test1";
-    stts.innerHTML = "Test2";
-    stgame.innerHTML = "Test3";
-    stwebsite.innerHTML = "Test4";
+    console.log(this.responseText);
   }
+});
+
+
+xhr.open("GET", "https://fearnixx.de/status/public/api/v1/components?sort=status&order=desc");
+
+xhr.send(data);
+
+
+
+//var type ids
+var stroot = document.getElementById('root');
+var stts = document.getElementById('ts');
+var stgame = document.getElementById('game');
+var stwebsite = document.getElementById('website');
+
+//var type infos
+//var clroot = ;
+//var clts = ;
+//var clgame = ;
+//var clwebsite = ;
+
+
+console.log(stroot.innerHTML, stts.innerHTML, stgame.innerHTML, stwebsite.innerHTML);
+
+if (this.readyState === this.DONE) {
+  stroot.innerHTML = "Test1";
+  stts.innerHTML = "Test2";
+  stgame.innerHTML = "Test3";
+  stwebsite.innerHTML = "Test4";
 }
 
 
-postState();
+
+
 
 //https://fearnixx.de/status/public/api/v1/components?sort=status=asc
 
