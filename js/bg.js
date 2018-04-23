87a6338ce1fea393aab4636c53b62671a7c75585
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2018-04-23 10:38:28 +0200

    cleaning code

--- js/bg.js
+++ js/bg.js
@@ -27,61 +27,61 @@ xhr.addEventListener("readystatechange", function () {
 
 
 
-function CreateTable(){
-  for (var i = 0; i < obj.data.length; i++) {
-    let component = obj.data[i];
-    let id = obj.data[i];
-    let name = obj.data[i].name;
-    let stat_name = obj.data[i].status_name;
-    //let idTable = document.getElementById('myTable');
+    function CreateTable(){
+      for (var i = 0; i < obj.data.length; i++) {
+        let component = obj.data[i];
+        let id = obj.data[i];
+        let name = obj.data[i].name;
+        let stat_name = obj.data[i].status_name;
+        //let idTable = document.getElementById('myTable');
 
-    function goodMorningV(stat_name) {
+        function goodMorningV(stat_name) {
 
-      $( document ).ready(function() {
-        if (stat_name === "Offline") {
-          alert("The Server " + name + " is Offline");
+          $( document ).ready(function() {
+            if (stat_name === "Offline") {
+              alert("The Server " + name + " is Offline");
+            }
+          });
+
         }
-      });
 
+        let thead = $('<th/>', {
+          id: "hid_" + i,
+          class: "status_head"
+        })
+
+        let tbody = $('<td/>', {
+          id: "id_" + i,
+          class: "status_body"
+        })
+
+        let trow = $('<tr>', {
+          id: "root_" + i
+        })
+
+        $('#myHead').append($(trow));
+
+        $('#myBody').append($(trow));
+
+        $('#root_' + i).append($(thead));
+
+        $('#root_' + i).append($(tbody));
+
+        $('#hid_' + i).append(name);
+
+        $('#id_' + i).append(stat_name);
+
+
+
+
+        //console.log();
+        globalos(id, i);
+      }
     }
 
-      let thead = $('<th/>', {
-        id: "hid_" + i,
-        class: "status_head"
-      })
 
-      let tbody = $('<td/>', {
-        id: "id_" + i,
-        class: "status_body"
-      })
-
-      let trow = $('<tr>', {
-        id: "root_" + i
-      })
-
-      $('#myHead').append($(trow));
-
-      $('#myBody').append($(trow));
-
-      $('#root_' + i).append($(thead));
-
-      $('#root_' + i).append($(tbody));
-
-      $('#hid_' + i).append(name);
-
-      $('#id_' + i).append(stat_name);
-
-
-
-
-    //console.log();
-    globalos(id, i);
+    setInterval(CreateTable(),3000);
   }
-}
-
-
-setInterval(CreateTable(),3000);
-}
 });
 
 
